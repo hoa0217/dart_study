202b50ca7354c70bf8b7a6cc2fb98819b5b9528d
hoa0217 <gjwjdghk123@naver.com>
2023-12-27 00:10:42 +0900

    feature: 2-9 가위바위보 앱 만들기

--- flutter_example/lib/main.dart
+++ flutter_example/lib/main.dart
@@ -1,23 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_example/screen/new_page.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Widget을 겹겹히 쌓아 배치하기'),
-        ),
-        body: Body(),
-      ),
+    MaterialApp.router(
+      routerConfig: GoRouter(initialLocation: '/', routes: [
+        GoRoute(
+            path: '/',
+            name: 'home',
+            builder: (context, _) => const HomeWidget()),
+        GoRoute(
+            path: '/new',
+            name: 'new',
+            builder: (context, _) => const NewPage()),
+        GoRoute(
+            path: '/new1',
+            name: 'new1',
+            builder: (context, _) => const NewPage2())
+      ]),
+      theme: customTheme,
     ),
   );
 }
 
-class Body extends StatelessWidget {
-  const Body({super.key});
+final customTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+  textTheme: const TextTheme(
+      bodyLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+  useMaterial3: true, // 기본적으로 3가 true.
+);
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final textTheme = customTheme.textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Theme'),
+      ),
+      body: Center(
+        child: TextButton(
+          child: Text(
+            'Go to Page',
+            style: textTheme.bodyLarge,
+          ),
+          onPressed: () => context.pushNamed('new'),
+        ),
+      ),
+    );
   }
 }
---
+++ flutter_rsp_game/lib/game/widget/user_input.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rsp_game/game/enum.dart';
+import 'package:flutter_rsp_game/game/widget/input_card.dart';
+
+class UserInput extends StatelessWidget {
+  final bool isDone;
+  final Function(InputType) callback;
+  final InputType? userInput;
+
+  const UserInput(
+      {required this.isDone,
+      required this.callback,
+      this.userInput,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (isDone) {
+      return Row(
+        children: [
+          const Expanded(
+            child: SizedBox.shrink(),
+          ),
+          Expanded(
+            child: InputCard(
+              child: Image.asset(userInput!.path),
+            ),
+          ),
+          const Expanded(
+            child: SizedBox.shrink(),
+          ),
+        ],
+      );
+    }
+
+    return Row(
+      children: _getInputs(callback),
+    );
+  }
+
+  List<Widget> _getInputs(Function(InputType) callback) {
+    return InputType.values
+        .map(
+          (type) => Expanded(
+            child: InkWell(
+              onTap: () => callback.call(type),
+              child: InputCard(
+                child: Image.asset(type.path),
+              ),
+            ),
+          ),
+        )
+        .toList();
+  }
+}
