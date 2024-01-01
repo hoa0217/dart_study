2a656197b802b5783b5534f0bd02eee51b856b68
hoa0217 <gjwjdghk123@naver.com>
2024-01-01 20:57:07 +0900

    feature: 2-7 Flutter에서 페이지에서 페이지 이동하기1 BottomNavigationBar 사용

---
+++ flutter_example/lib/2-7페이지에서페이지이동하기1.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeWidget(),
+    ),
+  );
+}
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
+
+  @override
+  State<HomeWidget> createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
+  late int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter에서 화면 이동하기'),
+      ),
+      body: homeBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          const BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          const BottomNavigationBarItem(icon: Icon(Icons.person), label: 'User'),
+        ],
+        currentIndex: index,
+        onTap: (newIndex) => setState(() {
+          index = newIndex;
+        }),
+      ),
+    );
+  }
+
+  Widget homeBody() {
+    switch (index) {
+      case 1:
+        return Center(
+          child: Icon(
+            Icons.search,
+            size: 100,
+          ),
+        );
+      case 2:
+        return Center(
+          child: Icon(
+            Icons.person,
+            size: 100,
+          ),
+        );
+      case 0:
+      default:
+        return Center(
+          child: Icon(
+            Icons.home,
+            size: 100,
+          ),
+        );
+    }
+  }
+}
